ee2607af8dc059ae8f414bfb7b8a9da58a3c0ac0
Alec Jang <ayj.max@gmail.com>
2024-07-14 22:17:24 -0400

    Trying QuillJS, CodeMirror, and more editors

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import {EditorState} from "@codemirror/state"
+  import {EditorView, keymap} from "@codemirror/view"
+  import {defaultKeymap} from "@codemirror/commands"
+
+  onMount(() => {
+    let startState = EditorState.create({
+    doc: "Hello World",
+    extensions: [keymap.of(defaultKeymap)]
+  })
+
+  let view = new EditorView({
+    state: startState,
+    parent: document.getElementById("codemirror-editor") ?? document.body
+  });
+  })
+</script>
+
+<svelte:head>
+	<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+</svelte:head>
+<main>
+  <h1>Welcome to MantaDocs</h1>
+  <!-- <div bind:this={editorContainer}></div> -->
+   <textarea id="editor"></textarea>
+   <div id="codemirror-editor"></div>
+</main>
+
+<style lang="scss">
+
+</style>
